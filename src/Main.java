769c6a354351bb8c2aa3dff913f7698f7bf5457b
Jai Bedia <jaibedia8@gmail.com>
2024-01-24 22:56:41 +0530

    included addition method

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    int a = 10;
+    int b = 2;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            System.out.println("i = " + i+2);
-        }
+    public static void add(int a, int b){
+            System.out.println("a+b ="+ a+b);
     }
 }
